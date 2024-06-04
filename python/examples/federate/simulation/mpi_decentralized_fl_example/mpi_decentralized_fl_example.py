908fba99a5ea34afbd584f7a199431cdfa1e4cc4
addalin <addalinqmul@gmail.com>
2024-06-04 09:33:37 +0300

    changes made to test algorithm and wandb

--- python/examples/federate/simulation/mpi_decentralized_fl_example/mpi_decentralized_fl_example.py
+++ python/examples/federate/simulation/mpi_decentralized_fl_example/mpi_decentralized_fl_example.py
@@ -1,7 +1,10 @@
 import fedml
 from fedml import FedMLRunner
+import wandb
 
 if __name__ == "__main__":
+
+    wandb.login(key='63a4dccbf22454ee89c03213ddaba326fa6d7460')
     # init FedML framework
     args = fedml.init()
 
