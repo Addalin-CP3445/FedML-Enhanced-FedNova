8c375c217f3ef8e531c149cf19f80cb91423eb84
Addalin <60094638@cna-qatar.edu.qa>
2024-07-08 19:15:53 +0300

    Added FedProx baseline

---
+++ python/examples/federate/simulation/FedProx_base_with_DP/mpi_fedprox.py
@@ -0,0 +1,22 @@
+import fedml
+from fedml import FedMLRunner
+import wandb
+
+if __name__ == "__main__":
+
+    wandb.login(key='63a4dccbf22454ee89c03213ddaba326fa6d7460')
+    # init FedML framework
+    args = fedml.init()
+
+    # init device
+    device = fedml.device.get_device(args)
+
+    # load data
+    dataset, output_dim = fedml.data.load(args)
+
+    # load model
+    model = fedml.model.create(args, output_dim)
+
+    # start training
+    fedml_runner = FedMLRunner(args, device, dataset, model)
+    fedml_runner.run()
